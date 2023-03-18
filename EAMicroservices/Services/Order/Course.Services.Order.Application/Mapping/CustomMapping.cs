7b527484be62c8db057d33c7219d1bf368e01a2d
emreakturk1125 <52127624+emreakturk1125@users.noreply.github.com>
2023-03-18 13:05:19 +0300

    Orrder Api  Development

---
+++ EAMicroservices/Services/Order/Course.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Course.Services.Order.Application.Dtos;
+using Course.Services.Order.Domain.OrderAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Services.Order.Application.Mapping
+{
+    internal class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
+        }
+    }
+}
---
+++ EAMicroservices/Services/Order/Course.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using Course.Services.Order.Application.Dtos;
+using Course.Shared.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Services.Order.Application.Queries
+{
+    public class GetOrdersByUserIdQuery : IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
